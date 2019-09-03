84d992355a0af601deb48addaba34644c2736424
Ort Moodle developer <MoodleDev@ort.org.il>
2019-09-03 14:17:52 +0300

    add Hebrew translation

---
+++ lang/he/atto_hvp.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_hvp', language 'en'.
+ *
+ * @package    atto_hvp
+ * @author     Lea Cohen <leac@ort.org.il>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['hvp'] = ' רכיבי ה-H5P במרחב';
+$string['inserthvp'] = 'להכניס H5P';
+$string['nohvps'] = 'מצטערים, לא נמצא פריט מסוג H5P.';
+$string['pluginname'] = 'H5P';
+$string['select_desc'] = 'בחרו את פריט ה-H5P אותו אתם מבקשים להטמיע.';
+$string['title'] = 'בחרו H5P';
